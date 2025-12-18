2ae89b79fb7592818e2bb2109b92757c6d4b7945
Ralfas Ragauskas <Ralfasr02@gmail.com>
2025-12-18 12:27:05 +0100

    EX 2.1

--- TimeSlot/Program.cs
+++ TimeSlot/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TimeSlot.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TimeSlotContext>(options => 
+{ options.UseSqlServer
+(builder.Configuration.GetConnectionString("TimeSlotConnection")); });
 
 var app = builder.Build();
 
